329e2093087585dafa93d525f028833b6e1b8e43
rscnt <rascnt@gmail.com>
2015-07-19 12:19:20 -0600

    emacs: python, install ropemacs and ropemacs_py3k

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -19,11 +19,11 @@
 ;; ================
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+	     '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
+	     '("org" . "http://orgmode.org/elpa/") t) ; Org-mode's repository
 (add-to-list 'package-archives
-             '("marmalade" . "http://marmalade-repo.org/packages/"))
+	     '("marmalade" . "http://marmalade-repo.org/packages/"))
 (when (< emacs-major-version 24)
   ;; For important compatibility libraries like cl-lib
   (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/")))
@@ -42,6 +42,9 @@
 (global-set-key (kbd "C-c j") 'windmove-down)
 ;; ====
 
+(require 'ido)
+(ido-mode t)
+
 (require 'cask "~/.cask/cask.el")
 (cask-initialize)
 (require 'pallet)
@@ -49,12 +52,12 @@
 
 ;;; Company mode:
 (setq company-backends '(company-elisp
-                          company-ropemacs
-                          company-gtags
-                          company-dabbrev-code
-                          company-keywords
-                          company-files
-                          company-dabbrev))
+			  company-ropemacs
+			  company-gtags
+			  company-dabbrev-code
+			  company-keywords
+			  company-files
+			  company-dabbrev))
 
 (add-hook 'after-init-hook 'global-company-mode)
 (put 'upcase-region 'disabled nil)
@@ -64,7 +67,7 @@
 (add-hook 'after-init-hook #'global-flycheck-mode)
 (require 'flycheck-color-mode-line)
 (eval-after-load "flycheck"
-                 '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
+		 '(add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
 
 ;;=============================
 ;; Linum
@@ -116,8 +119,8 @@
 (smartparens-global-mode)
 (show-smartparens-global-mode t)
 (sp-with-modes '(rhtml-mode)
-               (sp-local-pair "<" ">")
-               (sp-local-pair "<%" "%>"))
+	       (sp-local-pair "<" ">")
+	       (sp-local-pair "<%" "%>"))
 
 ;; ruby mode
 (add-hook 'ruby-mode-hook 'robe-mode)
@@ -135,15 +138,15 @@
 (require 'cl) ; If you don't have it already
 
 (defun* get-closest-gemfile-root (&optional (file "Gemfile"))
-        "Determine the pathname of the first instance of FILE starting from the current directory towards root.
-        This may not do the correct thing in presence of links. If it does not find FILE, then it shall return the name
-        of FILE in the current directory, suitable for creation"
-        (let ((root (expand-file-name "/"))) ; the win32 builds should translate this correctly
-          (loop
-            for d = default-directory then (expand-file-name ".." d)
-            if (file-exists-p (expand-file-name file d))
-            return d
-            if (equal d root) return nil)))
+	"Determine the pathname of the first instance of FILE starting from the current directory towards root.
+	This may not do the correct thing in presence of links. If it does not find FILE, then it shall return the name
+	of FILE in the current directory, suitable for creation"
+	(let ((root (expand-file-name "/"))) ; the win32 builds should translate this correctly
+	  (loop
+	    for d = default-directory then (expand-file-name ".." d)
+	    if (file-exists-p (expand-file-name file d))
+	    return d
+	    if (equal d root) return nil)))
 
 ;; prcjectile rails
 
@@ -177,32 +180,32 @@
 ;; For electric goodness!
 (require 'ruby-electric)
 (add-hook 'ruby-mode-hook (lambda ()
-                            (unless (derived-mode-p 'prog-mode)
-                              (run-hooks 'prog-mode-hook))
-                            (require 'ruby-electric)
-                            (ruby-electric-mode t)))
+			    (unless (derived-mode-p 'prog-mode)
+			      (run-hooks 'prog-mode-hook))
+			    (require 'ruby-electric)
+			    (ruby-electric-mode t)))
 
 (eval-after-load 'ruby-mode
-                 '(progn
-                    ;; work around possible elpa bug
-                    (ignore-errors (require 'ruby-compilation))
-                    (setq ruby-use-encoding-map nil)
-                    (autoload 'inf-ruby "inf-ruby")
-                    (add-hook 'ruby-mode-hook 'inf-ruby-keys)
-                    (autoload 'inf-ruby-switch-setup "inf-ruby")
-                    (add-hook 'ruby-mode-hook 'flymake-ruby-load)
-                    (require 'ruby-tools)
-                    (ruby-tools-mode +1)
-                    (require 'ruby-block)
-                    (ruby-block-mode t)
-                    (add-hook 'ruby-mode-hook 'ri-bind-key)
-                    (let ((m ruby-mode-map))
-                      (define-key m (kbd "RET") 'reindent-then-newline-and-indent)
-                      (define-key m (kbd "C-M-h") 'backward-kill-word)
-                      (define-key m (kbd "C-c l") "lambda")
-                      (define-key m [S-f7] 'ruby-compilation-this-buffer)
-                      (define-key m [f7] 'ruby-compilation-this-test)
-                      (define-key m [f8] 'recompile))))
+		 '(progn
+		    ;; work around possible elpa bug
+		    (ignore-errors (require 'ruby-compilation))
+		    (setq ruby-use-encoding-map nil)
+		    (autoload 'inf-ruby "inf-ruby")
+		    (add-hook 'ruby-mode-hook 'inf-ruby-keys)
+		    (autoload 'inf-ruby-switch-setup "inf-ruby")
+		    (add-hook 'ruby-mode-hook 'flymake-ruby-load)
+		    (require 'ruby-tools)
+		    (ruby-tools-mode +1)
+		    (require 'ruby-block)
+		    (ruby-block-mode t)
+		    (add-hook 'ruby-mode-hook 'ri-bind-key)
+		    (let ((m ruby-mode-map))
+		      (define-key m (kbd "RET") 'reindent-then-newline-and-indent)
+		      (define-key m (kbd "C-M-h") 'backward-kill-word)
+		      (define-key m (kbd "C-c l") "lambda")
+		      (define-key m [S-f7] 'ruby-compilation-this-buffer)
+		      (define-key m [f7] 'ruby-compilation-this-test)
+		      (define-key m [f8] 'recompile))))
 
 
 
@@ -244,17 +247,17 @@
 (add-hook 'before-save-hook 'gofmt-before-save)
 ; go-import-add is bound to C-c C-a by default
 (add-hook 'go-mode-hook '(lambda ()
-                           (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
+			   (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)))
 ; Now we can use C-c C-r to remove all unused imports, or C-u C-c C-r to comment out unused imports.
 (add-hook 'go-mode-hook '(lambda ()
-                           (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
+			   (local-set-key (kbd "C-c C-g") 'go-goto-imports)))
 ; format without save.
 (add-hook 'go-mode-hook '(lambda ()
-                           (local-set-key (kbd "C-c C-f") 'gofmt)))
+			   (local-set-key (kbd "C-c C-f") 'gofmt)))
 (add-hook 'before-save-hook 'gofmt-before-save)
 ; documentation
 (add-hook 'go-mode-hook '(lambda ()
-                           (local-set-key (kbd "C-c C-k") 'godoc)))
+			   (local-set-key (kbd "C-c C-k") 'godoc)))
 ; godef is a really nifty tool that parses go code
 ; and enables you to quickly jump the definition of any symbol or read its description
 ; go-mode provides two functions for interacting
@@ -283,8 +286,8 @@
 
 (add-hook 'go-mode-hook 'company-mode)
 (add-hook 'go-mode-hook (lambda ()
-                          (set (make-local-variable 'company-backends) '(company-go))
-                          (company-mode)))
+			  (set (make-local-variable 'company-backends) '(company-go))
+			  (company-mode)))
 
 ;; (require 'auto-complete-config)
 ;; (setq ac-auto-start t)
@@ -311,7 +314,7 @@
 (add-hook 'objc-mode-hook 'irony-mode)
 
 (eval-after-load 'company
-                 '(add-to-list 'company-backends 'company-irony))
+		 '(add-to-list 'company-backends 'company-irony))
 
 ;; (optional) adds CC special commands to `company-begin-commands' in order to
 ;; trigger completion at interesting places, such as after scope operator
@@ -338,11 +341,140 @@
 (add-hook 'pandoc-mode-hook 'pandoc-load-default-settings)
 
 (autoload 'markdown-mode "markdown-mode"
-          "Major mode for editing Markdown files" t)
+	  "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\\.text\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 
 (setq magit-auto-revert-mode nil)
 (setq magit-last-seen-setup-instructions "1.4.0")
+
+(defun rst-validate-buffer ()
+  "Tests to see if buffer is valid reStructured Text."
+  (interactive)
+  (if (eq major-mode 'rst-mode)         ; only runs in rst-mode
+      (let ((name (buffer-name))
+	    (filename (buffer-file-name)))
+	(cond
+	 ((not (file-exists-p "/usr/bin/rst2pseudoxml")) ; check that the program used to process file is present
+	      (error "Docutils programs not available."))
+	 ((not (file-exists-p filename)) ; check that the file of the buffer exists
+	      (error "Buffer '%s' is not visiting a file!" name))
+	 (t                             ; ok, process the file, producing pseudoxml output
+	  (let* ((pseudoxml (split-string (shell-command-to-string (concat "rst2pseudoxml " filename)) "\n"))
+		 (firstline (car pseudoxml)) ; take first line of output
+		 (lastline (nth (- (length pseudoxml) 2) pseudoxml))) ; take last line of output text
+	    (if (or (string-match "ERROR/" firstline)
+		    (string-match "WARNING/" firstline)
+		    ;; for reasons I don't understand, sometimes the warnings/errors are at the end of output
+		    (string-match "ERROR/" lastline)
+		    (string-match "WARNING/" lastline))
+		(progn (ding)
+		       (message "There's an problem in this buffer."))
+	      (message "Buffer is valid reStructured Text."))))))))
+
+(add-hook 'rst-mode-hook
+	  (lambda()
+	    (add-hook 'after-save-hook 'rst-validate-buffer)))
+
+;; find aspell and hunspell automatically
+(cond
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell")
+  (setq ispell-extra-args '("--sug-mode=ultra" "--lang=en_US")))
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  (setq ispell-extra-args '("-d en_US")))
+ )
+
+					; if (aspell installed) { use aspell}
+;; else if (hunspell installed) { use hunspell }
+;; whatever spell checker I use, I always use English dictionary
+;; I prefer use aspell because:
+;; 1. aspell is older
+;; 2. looks Kevin Atkinson still get some road map for aspell:
+;; @see http://lists.gnu.org/archive/html/aspell-announce/2011-09/msg00000.html
+(defun flyspell-detect-ispell-args (&optional RUN-TOGETHER)
+  "if RUN-TOGETHER is true, spell check the CamelCase words"
+  (let (args)
+    (cond
+     ((string-match  "aspell$" ispell-program-name)
+      ;; force the English dictionary, support Camel Case spelling check (tested with aspell 0.6)
+      (setq args (list "--sug-mode=ultra" "--lang=en_US"))
+      (if RUN-TOGETHER
+	  (setq args (append args '("--run-together" "--run-together-limit=5" "--run-together-min=2")))))
+     ((string-match "hunspell$" ispell-program-name)
+      (setq args nil)))
+    args
+    ))
+
+(cond
+ ((executable-find "aspell")
+  (setq ispell-program-name "aspell"))
+ ((executable-find "hunspell")
+  (setq ispell-program-name "hunspell")
+  ;; just reset dictionary to the safe one "en_US" for hunspell.
+  ;; if we need use different dictionary, we specify it in command line arguments
+  (setq ispell-local-dictionary "en_US")
+  (setq ispell-local-dictionary-alist
+	'(("en_US" "[[:alpha:]]" "[^[:alpha:]]" "[']" nil nil nil utf-8))))
+ (t (setq ispell-program-name nil)))
+
+;; ispell-cmd-args is useless, it's the list of *extra* arguments we will append to the ispell process when "ispell-word" is called.
+;; ispell-extra-args is the command arguments which will *always* be used when start ispell process
+(setq ispell-extra-args (flyspell-detect-ispell-args t))
+;; (setq ispell-cmd-args (flyspell-detect-ispell-args))
+(defadvice ispell-word (around my-ispell-word activate)
+  (let ((old-ispell-extra-args ispell-extra-args))
+    (ispell-kill-ispell t)
+    (setq ispell-extra-args (flyspell-detect-ispell-args))
+    ad-do-it
+    (setq ispell-extra-args old-ispell-extra-args)
+    (ispell-kill-ispell t)
+    ))
+
+(defadvice flyspell-auto-correct-word (around my-flyspell-auto-correct-word activate)
+  (let ((old-ispell-extra-args ispell-extra-args))
+    (ispell-kill-ispell t)
+    ;; use emacs original arguments
+    (setq ispell-extra-args (flyspell-detect-ispell-args))
+    ad-do-it
+    ;; restore our own ispell arguments
+    (setq ispell-extra-args old-ispell-extra-args)
+    (ispell-kill-ispell t)
+    ))
+
+(require 'fill-column-indicator)
+(define-globalized-minor-mode
+  global-fci-mode fci-mode (lambda () (fci-mode 1)))
+(global-fci-mode t)
+
+(require 'python-mode)
+			; use IPython
+(setq-default py-shell-name "ipython")
+(setq-default py-which-bufname "IPython")
+					; use the wx backend, for both mayavi and matplotlib
+(setq py-python-command-args
+      '("--gui=wx" "--pylab=wx" "-colors" "Linux"))
+(setq py-force-py-shell-name-p t)
+
+					; switch to the interpreter after executing code
+(setq py-shell-switch-buffers-on-execute-p t)
+(setq py-switch-buffers-on-execute-p t)
+					; don't split windows
+(setq py-split-windows-on-execute-p nil)
+					; try to automagically figure out indentation
+(setq py-smart-indentation t)
+
+(autoload 'pymacs-apply "pymacs")
+(autoload 'pymacs-call "pymacs")
+(autoload 'pymacs-eval "pymacs" nil t)
+(autoload 'pymacs-exec "pymacs" nil t)
+(autoload 'pymacs-load "pymacs" nil t)
+(autoload 'pymacs-autoload "pymacs")
+
+; ropemacs
+(require 'pymacs)
+(pymacs-load "ropemacs" "rope-")
+
 ;; init.el ends here.
